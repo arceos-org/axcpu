e8acc6d6f6a0674d6b8c429460f8303764cebdba
numpy1314 <xzj13@ustc.edu.cn>
2025-07-01 10:11:33 +0800

    remove useless comment

--- src/aarch64/asm.rs
+++ src/aarch64/asm.rs
@@ -118,21 +118,6 @@ pub unsafe fn write_user_page_table(root_paddr: PhysAddr) {
 ///
 /// If `vaddr` is [`None`], flushes the entire TLB. Otherwise, flushes the TLB
 /// entry that maps the given virtual address.
-///
-/// Moreover, The reason for shifting vaddr right by 12 bits here is to clear
-/// the address offset of the current position and obtain the aligned page number.
-///
-/// Other functions do not use page numbers(no need to shift right by 12 bits)?
-///
-/// ​Cache refresh: Data cache is in cache lines, and the full address is required
-/// to locate a specific cache line, not the page number.
-///
-/// Page table register write: When directly operating the TTBRx_ELx register,
-/// the physical address of the page table base address is required, and no virtual
-/// address displacement is required.
-///
-/// ​Exception vector table: When setting the exception handling entry, just write
-/// the base address directly.
 #[inline]
 pub fn flush_tlb(vaddr: Option<VirtAddr>) {
     unsafe {
