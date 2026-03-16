da5fc1705e7e8774748927ea6f70db6418572f42
Debin <luodebin@kylinos.cn>
2026-03-16 15:27:00 +0800

    add uspace for arceos clippy

--- src/arm/mod.rs
+++ src/arm/mod.rs
@@ -8,4 +8,13 @@ pub mod init;
 #[cfg(target_os = "none")]
 mod trap;
 
+#[cfg(feature = "uspace")]
+pub mod uspace {
+    // TODO: This module is currently empty, but it will contain user-space related code in the future.
+    use crate::TrapFrame;
+
+    /// Context to enter user space.
+    pub struct UspaceContext(TrapFrame);
+}
+
 pub use self::context::{FpState, TaskContext, TrapFrame};
