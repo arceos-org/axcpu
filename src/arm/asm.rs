24bd5d9c29c282cc31494e58d9dbdd81824976e3
Debin <luodebin@kylinos.cn>
2026-03-16 14:19:38 +0800

    add read/write_thread_pointer

--- src/arm/asm.rs
+++ src/arm/asm.rs
@@ -192,6 +192,27 @@ pub unsafe fn write_exception_vector_base(vbar: usize) {
     isb();
 }
 
+/// Reads the thread pointer of the current CPU (`TPIDRURW`).
+///
+/// It is used to implement TLS (Thread Local Storage).
+#[inline]
+pub fn read_thread_pointer() -> usize {
+    Tpidrurw::read().0 as usize
+}
+
+/// Writes the thread pointer of the current CPU (`TPIDRURW`).
+///
+/// It is used to implement TLS (Thread Local Storage).
+///
+/// # Safety
+///
+/// This function is unsafe as it changes the CPU states.
+#[inline]
+pub unsafe fn write_thread_pointer(tp: usize) {
+    unsafe { Tpidrurw::write(Tpidrurw(tp as u32)) };
+    isb();
+}
+
 /// Enable FP/SIMD instructions by setting the appropriate bits in CPACR.
 #[cfg(feature = "fp-simd")]
 #[inline]
